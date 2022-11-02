7c8d17bdd2630a4bc0a4976b62c80a5913d6be7f
Jakub Neumann <4jakub.neumann7@gmail.com>
2022-11-02 14:43:09 +0100

    autofind scheme.prisma using jq

--- bash/prisma-concatenate.sh
+++ bash/prisma-concatenate.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-prismaPath="./prisma/"
-schemaName="schema.prisma"
+#Variable setup
 directory="./src"
 
 #getopts is a built-in bash feature
-while getopts ':p:n:d:' OPTION; do
+while getopts ':d:' OPTION; do
     case $OPTION in
-        p)
-        prismaPath=$OPTARG
-        ;;
-        n)
-        schemaName=$OPTARG
-        ;;
         d)
         directory=$OPTARG
         ;;
         ?)
-        echo "script usage: $0 [-p <path to prisma>] [-n <schema name>] [-d <directory to search files in>]" >&2
+        echo "script usage: $0 [-d <directory to search files in>]" >&2
         exit 0
         ;;
     esac
 done
 
-schemaPath=$prismaPath$schemaName
+if [ $(cat package.json | jq '.prisma | has("schema")') == "true" ]
+then
+    schemaPath=$(cat package.json | jq -r '.prisma.schema')
+else
+    schemaPath="./prisma/schema.prisma"
+fi
 
 #Remove preexisting schema
 rm -f $schemaPath
+touch $schemaPath
 
 #Look for all .prisma files and concatenate them into the main file
-for file in `find ./src -type f -name "*.prisma"`; do
+for file in `find $directory -type f -name "*.prisma"`; do
     cat $file >> $schemaPath
 	echo "" >> $schemaPath
 done
